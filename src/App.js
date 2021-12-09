c93d663aca307c7797d29514bad5cc3b8ac41922
Lester Perez <lester14904@gmail.com>
2021-12-09 16:06:36 +0100

    el router es comentariado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 
 // Neceita mas modificaciones
 import React from 'react';
-
+import UsoDeMoment from "./component/UsoDeMoment"
+/*
 import { BrowserRouter, Switch,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RegisterForm from './component/RegisterForm';
 import Login from './component/Login';
 import UserList from './pages/UserList';
 import Layout from './component/Layout'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'*/
 function App() {
+  
   return (
+<div><UsoDeMoment/></div>
+
     // este es es el enrrutador 
-      <BrowserRouter>
+     /* <BrowserRouter>
         <Layout>
           <Switch>
               <Route exact path="/" component={Home} />
@@ -22,7 +26,7 @@ function App() {
               
           </Switch>
         </Layout>
-  </BrowserRouter>
+  </BrowserRouter>*/
   
      
      
