2ec13ccbe3aa2b5362df63ebc7716901a82df3bb
Lester Perez <lester14904@gmail.com>
2021-12-15 21:51:51 +0100

    creacion de componete user y actualisando el codigo de CreateUser

--- src/component/RegisterForm.js
+++ src/component/RegisterForm.js
@@ -1,36 +1,38 @@
 //este es el de register, validacion y errores
-
+// ver con manolito eel metodo createUser
 import React from "react";
 import { useForm } from "react-hook-form"; //libreria para trabajar con los formularios
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/ReactHook.css'
+//import CreateUser from '../servises/CreateUser'
 import moment from "moment";
-
 export default function RegisterForm() {
-  const time = moment()
-  const {register,reset,  handleSubmit, formState:{errors}} =  useForm();
+//const time = moment()
+  const {register,reset, handleSubmit, formState:{errors}} =  useForm();
 
-  const userList = JSON.parse(localStorage.getItem('localStorageUserList') || '[]') //compruevo si el localstore tine algun dat        //este funcion va a guardarme los datos en el localstorage      
-const  onSubmit = (data,) =>{
-      const user = userList.find(u => u.email === data.email);
-      if(user){alert("El usuario esta registrado")}
-        else{ //aca intoduje el operador spread(...) para asi agregar mas elemantos al array, como son los obeto de tipo date
-          userList.push({ ...data, userAdded:time.format(' dddd DD MMMM YYYY   HH:mm:ss'), userLoggedIn:"Sesion no iniciada aun"});
-            
-          localStorage.setItem('localStorageUserList',JSON.stringify(userList));
-          //esta funcion lo que haces es limpiar los campos despues de enviarlos al localstorage}
-        }
-     
-    reset({ 
-      fullName: '',
-      email: '',
-      password: '',
-      jobTitle: ''
-     
-    })
+
+const  onSubmit =(data)=>{
+    var time = moment();
+    var  userList = JSON.parse(localStorage.getItem('localStorageUserList') || '[]') //compruevo si el localstore tine algun dat        //este funcion va a guardarme los datos en el localstorage      
+    var  user = userList.find(u => u.email === data.email);
+    if(user){alert("El usuario esta registrado")}
+      else{
+          //aca intoduje el operador spread(...) para asi agregar mas elemantos al array, como son los obeto de tipo date
+        userList.push({ ...data, userAdded:time.format(' dddd DD MMMM YYYY   HH:mm:ss'), userLoggedIn:"Sesion no iniciada aun"});
+        localStorage.setItem('localStorageUserList',JSON.stringify(userList));
+        //esta funcion lo que haces es limpiar los campos despues de enviarlos al localstorage
+      }
+      reset({ 
+        fullName: '',
+        email: '',
+        password: '',
+        jobTitle: ''
+      })   
+    
+    
+    
     }
-   
-  
+
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     //el clasname es como react llama a los estyilos de css.
@@ -38,7 +40,7 @@ const  onSubmit = (data,) =>{
     <form  className="from-group Form_Style " onSubmit={handleSubmit(onSubmit)} >
       {/* register your input into the hook by invoking the "register" function */}
       <div>
-         <label>First Name</label> 
+        <label>First Name</label> 
          <input 
          className="form-control" 
         
---
+++ src/component/User.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class User extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div><h1>User</h1></div>
+                <div><span>Full Name: Lester </span>
+                     <span>Email:lester14904@gmail.com</span>   
+                     <span>Job Title:Software Engeenier</span>
+                     <span>User was Added : Software Engeenier</span>
+                     <span>last time User Login: Software Engeenier</span>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default User;
