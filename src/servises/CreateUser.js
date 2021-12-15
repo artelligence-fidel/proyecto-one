d9d4b8d86359573934eb8be2266e549d25ff93c9
Lester Perez <lester14904@gmail.com>
2021-12-15 21:47:45 +0100

    suviendo el servicion GreateUser

--- src/servises/CreateUser.js
+++ src/servises/CreateUser.js
@@ -1,3 +1,20 @@
 //VAS A COPIAR LO QUE EST AEN ELL REGISTER FOR, EN EL METODO ONSUBMIT PARA DESPUES PASARSELOS COMO UNA FUNCION APARTE
 import React from "react";
-
+//import { useForm } from "react-hook-form"; 
+import moment from "moment";
+export default function CreateUser(data){
+    var time = moment();
+    var  userList = JSON.parse(localStorage.getItem('localStorageUserList') || '[]') //compruevo si el localstore tine algun dat        //este funcion va a guardarme los datos en el localstorage      
+    var  user = userList.find(u => u.email === data.email);
+      if(user){alert("El usuario esta registrado")}
+      else{
+          //aca intoduje el operador spread(...) para asi agregar mas elemantos al array, como son los obeto de tipo date
+        userList.push({ ...data, userAdded:time.format(' dddd DD MMMM YYYY   HH:mm:ss'), userLoggedIn:"Sesion no iniciada aun"});
+        localStorage.setItem('localStorageUserList',JSON.stringify(userList));
+        //esta funcion lo que haces es limpiar los campos despues de enviarlos al localstorage
+      }
+         
+    
+    
+    
+    }
